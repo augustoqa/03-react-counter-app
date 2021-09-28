1f374cd59f901839725d6406ae70ca35b7d53b3c
Cesar <chechaacual@gmail.com>
2021-09-28 11:59:18 -0600

    Usar un Fragment para devolver varias etiquetas

--- src/PrimeraApp.jsx
+++ src/PrimeraApp.jsx
@@ -1,5 +1,12 @@
+import { Fragment } from 'react'
+
 const PrimeraApp = () => {
-  return <h1>Hola Mundo</h1>
+  return (
+    <>
+      <h1>Hola Mundo</h1>
+      <p>Mi primera aplicacion</p>
+    </>
+  )
 }
 
 export default PrimeraApp
