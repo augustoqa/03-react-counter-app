e586f03053de746739009f1fe4e7b47714fe2cd8
Cesar <chechaacual@gmail.com>
2021-09-28 19:48:19 -0600

    Comunicacion entre componentes - Props

--- src/PrimeraApp.jsx
+++ src/PrimeraApp.jsx
@@ -1,12 +1,9 @@
 import { Fragment } from 'react'
 
-const PrimeraApp = () => {
-  const saludo = 'Hola Mundo'
-
+const PrimeraApp = ({ saludo = 'Hola Mundo' }) => {
   return (
     <>
       <h1>{saludo}</h1>
-      {/* <pre>{JSON.stringify(saludo, null, 3)}</pre> */}
       <p>Mi primera aplicacion</p>
     </>
   )
