0ea1f93e5d0fa9c59c21a2fd8fa3281c74503ea1
Cesar <chechaacual@gmail.com>
2021-09-28 20:15:29 -0600

    Crear componente CounterApp

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import ReactDOM from 'react-dom'
-import PrimeraApp from './PrimeraApp'
+import CounterApp from './CouterApp'
 
 import './index.css'
 
 const divRoot = document.querySelector('#root')
 
-ReactDOM.render(<PrimeraApp saludo='Hola Mundo' />, divRoot)
+ReactDOM.render(<CounterApp value={10} />, divRoot)
