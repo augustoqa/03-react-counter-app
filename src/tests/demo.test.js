5cc29360834c8fc94c986b4bc670eee3eb1550d4
Cesar <chechaacual@gmail.com>
2021-10-18 10:07:01 -0600

    Usando toBe de Jest

--- src/tests/demo.test.js
+++ src/tests/demo.test.js
@@ -1,7 +1,12 @@
-test('debe de ser true', () => {
-  const isActive = true
+describe('Pruebas en el archivo demo.test.js', () => {
+  test('deben de ser iguales los strings', () => {
+    // 1. inicializacion
+    const mensaje = 'Hola Mundo'
 
-  if (isActive) {
-    throw new Error('No esta activo')
-  }
+    // 2. estimulo
+    const mensaje2 = `Hola Mundo`
+
+    // 3. Observar el comportamiento
+    expect(mensaje).toBe(mensaje2)
+  })
 })
