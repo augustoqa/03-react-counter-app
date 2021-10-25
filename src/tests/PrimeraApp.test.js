2037031e182ef6b0960329af654856c1684f82f2
Cesar <chechaacual@gmail.com>
2021-10-25 11:32:54 -0600

    Configurar Enzyme y enzyme-to-json

--- src/tests/PrimeraApp.test.js
+++ src/tests/PrimeraApp.test.js
@@ -1,12 +1,11 @@
-import { render } from '@testing-library/react'
+import { shallow } from 'enzyme'
+import '@testing-library/jest-dom'
 import PrimeraApp from '../PrimeraApp'
 
 describe('Pruebas en <PrimeraApp />', () => {
-  test('debe de mostrar el mensaje "Hola, Soy Bart"', () => {
+  test('debe de mostrar <PrimeraApp /> correctamente', () => {
     const saludo = 'Hola, Soy Bart'
-
-    const { getByText } = render(<PrimeraApp saludo={saludo} />)
-
-    expect(getByText(saludo)).toBeInTheDocument()
+    const wrapper = shallow(<PrimeraApp saludo={saludo} />)
+    expect(wrapper).toMatchSnapshot()
   })
 })
